853e358e06fdeb748c50f4e8b06de5a7ef227e8c
Marcell Gyurjan <m.gyurjan@tricentis.com>
2021-07-24 20:25:42 +0200

    disposal of unmanaged excel resources

--- ExcelParser/ExcelReader.cs
+++ ExcelParser/ExcelReader.cs
@@ -1,31 +1,70 @@
-﻿using Excel = Microsoft.Office.Interop.Excel;
+﻿using System;
+using Excel = Microsoft.Office.Interop.Excel;
+using Marshal = System.Runtime.InteropServices.Marshal;
 
 namespace ExcelParser {
-    public class ExcelReader {
+    public class ExcelReader : IDisposable {
         private Excel.Application xlApp;
+        private Excel.Workbooks xlWorkbooks;
         private Excel.Workbook xlWorkbook;
+        private Excel.Sheets xlWorksheets;
         private Excel._Worksheet xlWorksheet;
+        private Excel.Range xlRange;
 
         private string filePath;
         private string sheetName;
+        private bool isDisposed;
 
         public ExcelReader(string filePath, string sheetName) {
             this.filePath = filePath;
             this.sheetName = sheetName;
 
             xlApp = new Excel.Application();
-            xlWorkbook = xlApp.Workbooks.Open(filePath);
-            xlWorksheet = (Excel._Worksheet)xlWorkbook.Sheets[sheetName];
+            xlWorkbooks = xlApp.Workbooks;
+            xlWorkbook = xlWorkbooks.Open(filePath);
+            xlWorksheets = xlWorkbook.Worksheets;
+            xlWorksheet = xlWorksheets[sheetName];
         }
 
-        public string GetKanji(int i, int j) {
+        public string GetKanji(string range) {
             string cellValue = null;
-            var cellValueVar = (xlWorksheet.Cells[i, j] as Excel.Range).Value;
+            xlRange = xlWorksheet.get_Range(range);
+            var cellValueVar = xlRange.Value;
             if (cellValueVar != null) {
                 cellValue = cellValueVar.ToString();
             }
 
             return cellValue;
+        }
+
+        public void Dispose() {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) {
+            if (isDisposed) {
+                return;
+            }
+
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+            Marshal.ReleaseComObject(xlWorksheets);
+            xlWorkbook.Save();
+            xlWorkbook.Close(true);
+            Marshal.ReleaseComObject(xlWorkbook);
+            Marshal.ReleaseComObject(xlWorkbooks);
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+            xlRange = null;
+            xlWorksheet = null;
+            xlWorksheets = null;
+            xlWorkbook = null;
+            xlWorkbooks = null;
+            xlApp = null;
+
+            isDisposed = true;
         }
     }
 }
--- JPVocabularyManager/Program.cs
+++ JPVocabularyManager/Program.cs
@@ -6,10 +6,12 @@ using System.Web;
 namespace JPVocabularyManager {
     public class Program {
         static void Main(string[] args) {
+            string kanji;
+            using (ExcelReader excelReader = new ExcelReader(@"C:\Users\margy\Desktop\laptop\im agile.xlsx", "漢字")) {
+                 kanji = excelReader.GetKanji("B4");
+            }
+            
             ScriptParser scriptParser = new ScriptParser(Constants.PythonScriptPath);
-
-            ExcelReader excelReader = new ExcelReader(@"C:\Users\margy\Desktop\laptop\im agile.xlsx", "漢字");
-            string kanji = excelReader.GetKanji(4, 2);
             string url = string.Format(@"https://jisho.org/search/{0}/%20%23kanji", HttpUtility.UrlEncode(kanji));
 
             Console.Write(scriptParser.RunScript(url));
