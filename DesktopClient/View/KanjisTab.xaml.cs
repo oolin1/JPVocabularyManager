e7a4832092aa1ca706fa782441ef43f3d0414c40
Marcell Gyurjan <m.gyurjan@tricentis.com>
2021-08-30 23:43:15 +0200

    designed basic layout for Kanjis tab

---
+++ DesktopClient/View/KanjisTab.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace DesktopClient.View {
+    public partial class KanjisTab : UserControl {
+        public KanjisTab() {
+            InitializeComponent();
+        }
+    }
+}
